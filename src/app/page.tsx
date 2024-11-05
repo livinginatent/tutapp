0fccf0273f5a7b74fc4678c146fe8d3e9c3b3294
livinginatent <suleyman.eminbeyli@gmail.com>
2024-11-05 09:32:05 +0400

    added tut mascot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import Image from "next/image";
 import construction from "../../Under-Construction-Icon-PNG.png";
+import SVGComponent from "./tut";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Image
-        src={construction}
-        width={300}
-        height={300}
-        alt="under-construction"
-      />
+     <SVGComponent/>
       <h1 className="text-2xl">Under construction</h1>
       <p className="text-center">
         Our website is currently under construction to bring you an enhanced
